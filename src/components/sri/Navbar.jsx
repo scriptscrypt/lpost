7129bf65990e36a881416e25a18e8e5a2d8e91a0
scriptscrypt <tirumaladasa@gmail.com>
2023-04-09 10:16:14 +0530

    remoteV1 9Apr2023

--- src/components/sri/Navbar.jsx
+++ src/components/sri/Navbar.jsx
@@ -2,7 +2,8 @@
 //  @Sri - 6Apr2023
 //
 
-import {Button, Text, Modal} from "@mantine/core"
+import { useState } from "react";
+import {Button, Text, Modal, Avatar, Center, Flex, Box} from "@mantine/core"
 import { useDisclosure } from '@mantine/hooks';
 import { IconDatabase, IconShare, IconUpload, IconWallet } from '@tabler/icons-react';
 
@@ -11,6 +12,7 @@ import "./StylesSri.css"
 
 export default function NavbarImplement() {
   // const [openedModal, { openModal, closeModal }] = useDisclosure(false);
+  const [stOpenModal, setStOpenModal] = useState(false)
  
   // Modal Implementation 
   //
@@ -32,15 +34,45 @@ export default function NavbarImplement() {
       {/* Right Side of the Navbar */}
       <div className="">
 
-        <Button onClick={openModal} variant="subtle" m={4} leftIcon={<IconUpload size="1rem" />}> Upload </Button> 
+        <Button onClick={() => setStOpenModal(true)} variant="subtle" m={4} leftIcon={<IconUpload size="1rem" />}> Upload </Button> 
         <Button variant="subtle" m={4} leftIcon={<IconShare size="1rem" />}> Share </Button> 
-        <Button variant="filled" m={4} leftIcon={<IconWallet size="1rem" />}> Connect Wallet </Button> 
+        {/* <Button variant="filled" m={4} leftIcon={<IconWallet size="1rem" />}> Connect Wallet </Button>  */}
+        <Button variant="filled" m={4} > Connect Wallet </Button> 
       </div>
     
   </div>
 
-    <Modal opened={openedModal} onClose={closeModal} title="Upload" centered>
-        <Text>Modal Content</Text>
+    <Modal opened={stOpenModal} onClose={() =>{setStOpenModal(!stOpenModal)}} title="Upload" centered>
+        {/* <Text>Modal Content</Text> */}
+        
+        {/* <Center maw={400} h={100} mx="auto"> */}
+        <Flex
+            mih={80}
+            gap="lg"
+            justify="center"
+            align="center"
+            direction="row"
+            wrap="wrap"
+        >
+          <Box bg={"#fafafa"} >
+            <Avatar size={40} color="blue">D</Avatar>
+            <Text m={8}>Lens DP</Text>
+          </Box >
+
+          <Box bg={"#fafafa"} >
+            <Avatar size={40} color="blue">C</Avatar>
+            <Text m={8}>Lens Cover Picture</Text>
+          </Box >
+
+          <Box bg={"#fafafa"} >
+            <Avatar size={40} color="blue">N</Avatar>
+            <Text m={8}>Follower NFT</Text>
+          </Box >
+
+      </Flex> 
+       
+        {/* </Center> */}
+
     </Modal> 
     </>
   )
